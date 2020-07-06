ad0d5ec59310239eab90e07f673fe651a90b9d04
Aaron Sonderegger <aaronsonderegger@gmail.com>
2020-07-06 10:24:44 -0600

    All files that have been logged added

--- circuitCallibration/circuitCallibration.ino
+++ circuitCallibration/circuitCallibration.ino
@@ -1,7 +1,7 @@
 #define BAUD_RATE 9600
 #define STRAIN_OUTPUT A0
 #define SPRING_OUTPUT A1
-String ANGLE = "A+110";
+String ANGLE = "A30";
 
 unsigned int Start;
 unsigned int End;
@@ -14,11 +14,11 @@ void loop() {
   
 //  Serial.print( analogRead(STRAIN_OUTPUT) );
 //  Serial.print(" ");
-//  Serial.println( analogRead(SPRING_OUTPUT) ); 
-
-  Serial.print("SG_"+ANGLE+":");
-  Serial.print( analogRead(STRAIN_OUTPUT) );
-  Serial.print(",SP_"+ANGLE+":");
   Serial.println( analogRead(SPRING_OUTPUT) ); 
+//
+//  Serial.print("SG_"+ANGLE+":");
+//  Serial.print( analogRead(STRAIN_OUTPUT) );
+//  Serial.print(",SP_"+ANGLE+":");
+//  Serial.println( analogRead(SPRING_OUTPUT) ); 
   
 }
